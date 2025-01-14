e6189a0cd3c9c96426f9de109a37b6b97b115e59
Kavya Singh <kavyasingh19052005@gmail.com>
2025-01-15 00:19:06 +0530

    frontend completed

--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = ({setShowLogin}) => {
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
                 <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-                {/* <div className={getTotalCartAmount()===0?'':'dot'}></div> */}
+                <div className={getTotalCartAmount()===0?'':'dot'}></div> 
             </div>
             {!token?<button onClick={()=> setShowLogin(true)}>sign in</button>
             :<div className='navbar-profile'>
