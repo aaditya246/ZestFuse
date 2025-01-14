b004be50afaf4d31b7d8ca3e1fdd1a10902b8234
Kavya Singh <kavyasingh19052005@gmail.com>
2025-01-14 17:12:27 +0530

    Login and signup page

--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from './../../assets/assets';
 import {Link} from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext';
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
   const [menu, setMenu] = useState('home');
 
