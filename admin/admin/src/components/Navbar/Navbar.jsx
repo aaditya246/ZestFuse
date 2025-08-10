330d315cae13ebc7d0346242cde0b5955686fa11
kavyasingh12345 <166860159+kavyasingh12345@users.noreply.github.com>
2025-08-10 19:04:20 +0000

    fixed image

--- admin/admin/src/components/Navbar/Navbar.jsx
+++ admin/admin/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { assets } from './../../assets/assets';
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img className='logo' src={assets.logo} alt="" />
+        <h1 style={{ color: 'orange', margin: 0 }}>Zestfuse</h1>
         <img src={assets.profile_image} alt="" className="profile" />
     </div>
   )
